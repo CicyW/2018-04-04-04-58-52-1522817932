ece3af52d73412e2825c9036cf391a20a79d1905
CicyW <wangchi327@163.com>
2018-04-20 08:53:52 +0800

    Refactoring Order and PriceCaculator in practice3

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -9,30 +9,42 @@ public class Order {
     private List<BigDecimal> discounts;
     private BigDecimal tax;
 
+
     public Order(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts) {
         this.orderLineItemList = orderLineItemList;
         this.discounts = discounts;
         this.tax = new BigDecimal(0.1);
     }
 
+    public List<OrderLineItem> getOrderLineItemList() {
+        return orderLineItemList;
+    }
+
+    public List<BigDecimal> getDiscounts() {
+        return discounts;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setOrderLineItemList(List<OrderLineItem> orderLineItemList) {
+        this.orderLineItemList = orderLineItemList;
+    }
+
+    public void setDiscounts(List<BigDecimal> discounts) {
+        this.discounts = discounts;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
     public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
-
-        // Total up line items
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-
-        // Subtract discounts
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-
-        // calculate tax
-        BigDecimal tax = subTotal.multiply(this.tax);
+        PriceCaculator caculator = new PriceCaculator(this);
 
         // calculate GrandTotal
-        BigDecimal grandTotal = subTotal.add(tax);
+        BigDecimal grandTotal = caculator.calculateTotal();
 
         return grandTotal;
     }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,45 @@
 package practice3;
 
+import java.math.BigDecimal;
+
 public class PriceCaculator {
+
+    private Order order;
+
+    public PriceCaculator(Order order){
+        this.order = order;
+    }
+
+    public BigDecimal calculateSubTotal() {
+        BigDecimal subTotal = new BigDecimal(0);
+        for(OrderLineItem item:order.getOrderLineItemList()){
+            subTotal = subTotal.add(item.getPrice());
+        }
+        return subTotal;
+    }
+
+    public BigDecimal calculateDiscountTotal(){
+        BigDecimal discountTotal = new BigDecimal(0);
+        for(BigDecimal discount:order.getDiscounts()){
+            discountTotal = discountTotal.add(discount);
+        }
+        return discountTotal;
+    }
+
+    public BigDecimal calculateTotal(){
+        // calculate subTotal
+        BigDecimal subTotal = calculateSubTotal();
+
+        // calculate discount
+        BigDecimal discountTotal = calculateDiscountTotal();
+
+        subTotal = subTotal.subtract(discountTotal);
+
+        // calculate tax
+        BigDecimal tax = subTotal.multiply(order.getTax());
+        BigDecimal grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
+
 }
